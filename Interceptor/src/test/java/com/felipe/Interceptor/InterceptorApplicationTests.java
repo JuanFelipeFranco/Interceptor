f537b41eba7484f9bd9c390e06e5290c36f80337
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-06-14 11:20:56 -0500

    Initial commit

---
+++ Interceptor/src/test/java/com/felipe/Interceptor/InterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.Interceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterceptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
